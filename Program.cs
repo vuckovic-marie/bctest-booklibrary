cb11545a514476916ab634b1b19c2ea7cebdcaab
Marija Vuckovic <marija.v@codemancystudio.com>
2023-06-09 10:57:07 +0200

    Console app test
    Console app for fetching book data + storing basic data to a .txt file

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using BookLibrary;
+using BookLibrary.Services;
+
+Console.WriteLine("Hello, there!");
+Console.WriteLine("Do you want to get all the books from the library? (Y/N)");
+string? answer = Console.ReadLine();
+if (answer?.ToLower() == "y")
+{
+    try
+    {
+        List<Book> initBookList = await new BookService().GetAllBooks();
+        List<List<Book>> filteredBookList = await new BookService().FilterBooks(initBookList);
+        if (filteredBookList.Any())
+        {
+            string bookset = string.Empty;
+            foreach (var bookList in filteredBookList)
+            {
+                bookset += bookList.FirstOrDefault().parent_name + Environment.NewLine;
+                foreach (var book in bookList)
+                {
+                    bookset += book.display_name + " " + String.Join(",", book.meta.states) + Environment.NewLine;
+                }
+            }
+            new BookService().SaveBooks(bookset);
+        }
+        Console.WriteLine("All done! Check project folder for books.txt for data on books.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+else
+{
+    Console.ReadLine();
+}
+
+
---
+++ Services/IBookService.cs
@@ -0,0 +1,8 @@
+namespace BookLibrary.Services;
+
+public interface IBookService
+{
+    Task<List<Book>> GetAllBooks();
+    Task<List<List<Book>>> FilterBooks(List<Book> bookList);
+    void SaveBooks(string Bookset);
+}
